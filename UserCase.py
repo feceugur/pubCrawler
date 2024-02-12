1944679c3d40815b778ec0f686a4304631a120cc
fuldeneceugur <fulden@4securitas.com>
2024-02-12 17:39:16 +0100

    concatination added

--- UserCase.py
+++ UserCase.py
@@ -4,7 +4,6 @@ from KeyphraseExtraction import KeyphraseExtraction
 from PlaceSearch import PlaceSearch
 from SentimentExtractor import SentimentExtractor
 
-
 keyphrase_extractor = KeyphraseExtraction("davanstrien/deberta-v3-base_fine_tuned_food_ner")
 place_search = PlaceSearch('data.csv', keyphrase_extractor)
 sentiment_extractor = SentimentExtractor()
@@ -26,6 +25,7 @@ else:
     print("No matching places found.")
 """
 
+
 def extract_highest_score_label(sentiments):
     column_names = ['negative', 'neutral', 'positive']
     total_scores = []
@@ -42,13 +42,15 @@ def extract_highest_score_label(sentiments):
     return df_sentiments
 
 
-
 if matching_places is not None:
     matching_places = matching_places.reset_index(drop=True)
     sentiment_values = extract_highest_score_label(matching_places["sentiment"])
 
     df_concat = pd.concat([matching_places, sentiment_values], axis=1)
-    print(df_concat.groupby(['place_name']).mean())
+    df_concat = df_concat.drop(columns=['sentiment'])
+    df_concat = df_concat.groupby('place_name').mean(numeric_only=True)
+    df_concat['count'] = matching_places['place_name'].value_counts()
+    print(df_concat)
 
 
 else:
